3af2c594294177364af4e3607c74aff1a5218d7b
willnickerson <wnickers@willamette.edu>
2016-08-17 14:05:05 -0700

    basic for loop working

--- app.js
+++ app.js
@@ -4,6 +4,36 @@ alert('Hi! My name is Will. Please take a moment to read the directions on my pa
 
 var counter = 0; /* This var will tally the user's score */
 
+//random number questions
+
+var randomNum = Math.floor(Math.random() * 11);
+
+console.log('This is how many cups I\'ve had today: ' + randomNum);
+
+var userGuess = prompt('Can you guess how many cups of cofee I\'ve had today? I\'ll give you a hint. Its between 1 and 10, and I\'ll give you four tries. Good luck! ');
+
+userGuess = Number(userGuess);
+
+for(var i = 0; i < 4; i++) {
+  if (userGuess === randomNum) {
+    console.log('The user has guessed correctly. The loop should stop');
+    alert('You\'re right!');
+    i = 4; //this should stop the loop from running after they guess correctly
+    score++;
+  }
+  else {
+    var tries = 3 - i;
+    tries = tries.toString();
+    console.log('The user\'s guess is wrong.');
+    if (i < 3) {
+      userGuess = prompt('Guess again! You have ' + tries + ' more tries.');
+    } else {
+      userGuess = prompt('Guess again! This is your last chance...');
+    }
+    userGuess = Number(userGuess);
+  }
+}
+
 /* First Question */
 var anotherCupAnswer = prompt('Would you like another cup of coffee (yes or no)?');
 anotherCupAnswer = anotherCupAnswer.toUpperCase();
@@ -86,6 +116,7 @@ if (smokeAnswer === 'YES' || smokeAnswer === 'Y') {
   alert('I don\'t understand your answer and don\'t have time for someone who doesn\'t bother to read instructions');
   console.log('The user is not following instructions :(');
 }
+
 
 var score = counter / 5;
 if (counter >= 3) {
